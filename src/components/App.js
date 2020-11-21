e49d547f84732036d585ae41f6f4947507a5d907
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-21 20:26:55 +0530

    trail 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,54 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import View from "./View";
 const App = (props) => {
   const { slides } = props;
   //console.log(props);
   //console.log(slides.length);
 
-  const [show, setShow] = useState({
-    index: 0,
-    title: slides[0].title,
-    text: slides[0].text
-  });
+  const [index, setIndex] = useState(0);
+
+  let title = slides[index].title;
+  let text = slides[index].text;
 
   function handleClick(action) {
     //console.log(action);
     if (action === "prev") {
       //if (show.index === 0) return;
 
-      let index = show.index - 1;
-      // console.log(index);
-      // console.log(slides[index].title);
-
-      let title = slides[index].title;
-      let text = slides[index].text;
-
-      setShow({
-        index,
-        title,
-        text
-      });
+      let newIndex = index - 1;
+      setIndex(newIndex);
     } else if (action === "next") {
       //if (show.index === slides.length - 1) return;
 
-      let index = show.index + 1;
-      let title = slides[index].title;
-      let text = slides[index].text;
-
-      setShow({
-        index,
-        title,
-        text
-      });
+      let newIndex = index + 1;
+      setIndex(newIndex);
     } else if (action === "restart") {
-      setShow({
-        index: 0,
-        title: slides[0].title,
-        text: slides[0].text
-      });
+      setIndex(0);
     }
   }
 
   return (
     <>
-      <View key={show.index} title={show.title} text={show.text} />
+      <h1 data-testid="title">{title}</h1>
+      <p data-testid="text">{text}</p>
       <button
         onClick={() => handleClick("prev")}
         data-testid="button-prev"
-        disabled={show.index === 0 ? true : false}
+        disabled={index === 0 ? true : false}
       >
         Prev
       </button>
       <button
         onClick={() => handleClick("next")}
         data-testid="button-next"
-        disabled={show.index === slides.length - 1 ? true : false}
+        disabled={index === slides.length - 1 ? true : false}
       >
         Next
       </button>
       <button
         onClick={() => handleClick("restart")}
         data-testid="button-restart"
-        disabled={show.index === 0 ? true : false}
+        disabled={index === 0 ? true : false}
       >
         Restart
       </button>
