be808ac029327916cc2b010b57a048c4085128f5
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-21 19:14:39 +0530

    Trail 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,72 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import View from "./View";
+const App = (props) => {
+  const { slides } = props;
+  //console.log(props);
+  //console.log(slides.length);
 
-const App = () => {
+  const [show, setShow] = useState({
+    index: 0,
+    title: slides[0].title,
+    text: slides[0].text
+  });
+
+  function handleClick(action) {
+    //console.log(action);
+    if (action === "prev") {
+      if (show.index === 0) return;
+
+      let index = show.index - 1;
+      // console.log(index);
+      // console.log(slides[index].title);
+
+      let title = slides[index].title;
+      let text = slides[index].text;
+
+      setShow({
+        index,
+        title,
+        text
+      });
+    } else if (action === "next") {
+      if (show.index === slides.length - 1) return;
+
+      let index = show.index + 1;
+      let title = slides[index].title;
+      let text = slides[index].text;
+
+      setShow({
+        index,
+        title,
+        text
+      });
+    } else if (action === "restart") {
+      setShow({
+        index: 0,
+        title: slides[0].title,
+        text: slides[0].text
+      });
+    }
+  }
+
   return (
-    <></>
-  )
-}
-
+    <>
+      <View key={show.index} title={show.title} text={show.text} />
+      <button onClick={() => handleClick("prev")} data-testid="button-prev">
+        Prev
+      </button>
+      <button onClick={() => handleClick("next")} data-testid="button-next">
+        Next
+      </button>
+      <button
+        onClick={() => handleClick("restart")}
+        data-testid="button-restart"
+      >
+        Restart
+      </button>
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import slides from './data.js';
+import slides from "./data.js";
 
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+//console.log(slides);
+
+ReactDOM.render(<App slides={slides} />, document.getElementById("root"));
